b17ae69466366b2c2951eae48f9b201bf4bbed83
Richard Westmoreland <richard@westmorelandcreative.com>
2026-09-16 20:49:57 +0200

    feat: add migration files for updated pilots and flights collections

---
+++ pocketbase/base/migrations/1789584518_updated_pilots.go
@@ -0,0 +1,60 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2851445954")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"cascadeDelete": true,
+			"collectionId": "_pb_users_auth_",
+			"help": "",
+			"hidden": false,
+			"id": "relation2375276105",
+			"maxSelect": 0,
+			"minSelect": 0,
+			"name": "user",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2851445954")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "_pb_users_auth_",
+			"help": "",
+			"hidden": false,
+			"id": "relation2375276105",
+			"maxSelect": 0,
+			"minSelect": 0,
+			"name": "user",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
